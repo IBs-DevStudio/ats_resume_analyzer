77447150f9299bb3c857b52a7338a9ff863fc393
Ikram Banadar <ikrambanadar007@gmail.com>
2026-04-24 01:48:05 +0530

    Job Fit version 12 bugg

--- app/routes/landing.tsx
+++ app/routes/landing.tsx
@@ -1099,7 +1099,7 @@ export default function Landing() {
                           d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
                         />
                       </svg>
-                      88/100 ATS Score
+                      85/100 ATS Score
                     </div>
 
                     {/* Floating time badge */}
